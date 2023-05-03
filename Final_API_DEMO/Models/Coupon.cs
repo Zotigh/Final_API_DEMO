d7a2cdc25876734979998fb233d0bd758f6997bc
Zotigh <lzotigh1@cnm.edu>
2023-05-02 22:03:37 -0600

    Copied the models folder.

---
+++ Final_API_DEMO/Models/Coupon.cs
@@ -0,0 +1,28 @@
+﻿/******************************************************
+* Programmer: Lance Zotigh (lzotigh1@cnm.edu/lzotigh1@gmail.com)
+* Program: API Demo
+* Purpose: Demo that shows how to create a Minimal API
+*******************************************************/
+
+/*We are going to use a store for this project but ideally a database should be used to keep all of the coupons*/
+
+namespace DemoAPI.Models
+{
+    //Class for attributes
+    public class Coupon
+    {
+        public int Id { get; set; }
+        
+        public string? Name { get; set; }
+
+        public int Percent { get; set; }
+
+        public bool IsActive { get; set; }
+
+        //The "?" tells the compiler that this field is nullable which should satisfy the compiler since it knows.
+        public DateTime? Created { get; set; }
+
+        //The "?" tells the compiler that this field is nullable which should satisfy the compiler since it knows.
+        public DateTime? LastUpdated { get; set; }
+    }
+}
---
+++ Final_API_DEMO/Models/DTO/CouponDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoAPI.Models.DTO
+{
+    //This DTO just makes it so we cannot see when the coupon was last updated.
+    public class CouponDTO
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Percent { get; set; }
+
+        public bool IsActive { get; set; }
+
+        //The "?" tells the compiler that this feild is nullable which should satisfy the compiler since it knows.
+        public DateTime? Created { get; set; }
+    }
+}
