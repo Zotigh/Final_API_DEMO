e23da32f4d7e4f3df79447ce21c5e3953f6c3388
Zotigh <lzotigh1@cnm.edu>
2023-05-02 22:01:29 -0600

    Coppied Mapping class over.

---
+++ Final_API_DEMO/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DemoAPI.Models;
+using DemoAPI.Models.DTO;
+
+namespace DemoAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            //This mapps the coupon to CouponCreateDTO. The Reverse map allows the Coupon object
+            //to be mapped to another class.
+            CreateMap<Coupon, CouponCreateDTO>().ReverseMap();
+            CreateMap<Coupon, CouponDTO>().ReverseMap();         
+        }
+    }
+}
