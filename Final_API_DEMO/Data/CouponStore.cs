d5c560a3f62071e598c32f44ee1cf2b395388dd6
Zotigh <lzotigh1@cnm.edu>
2023-05-02 22:03:58 -0600

    Copied Data folder over.

---
+++ Final_API_DEMO/Data/CouponStore.cs
@@ -0,0 +1,20 @@
+﻿/******************************************************
+* Programmer: Lance Zotigh (lzotigh1@cnm.edu/lzotigh1@gmail.com)
+* Program: API Demo
+* Purpose: Demo that shows how to create a Minimal API
+*******************************************************/
+
+using DemoAPI.Models;
+
+namespace DemoAPI.Data
+{
+    //Made this a static class so we do not have to create this as an object every time.
+    public static class CouponStore
+    {
+        public static List<Coupon> couponList = new List<Coupon>()
+        {
+            new Coupon { Id = 1, Name = "10OFF", Percent = 10, IsActive = true },
+            new Coupon { Id = 2, Name = "20OFF", Percent = 20, IsActive = false }
+        };
+    }
+}
