e32f487562a064be8d64f6777843220393718ce3
Zotigh <lzotigh1@cnm.edu>
2023-05-02 22:00:48 -0600

    Copied the entire DTO folder over from other project.

--- Final_API_DEMO/DTO/CouponDeleteDTO.cs
+++ Final_API_DEMO/DTO/CouponDeleteDTO.cs
@@ -1,11 +1,10 @@
 ﻿namespace DemoAPI.Models.DTO
 {
-    public class CouponCreateDTO
+    public class CouponDeleteDTO
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
-
         public int Percent { get; set; }
-
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set;}
     }
 }
