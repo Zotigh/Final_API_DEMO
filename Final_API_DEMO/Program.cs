bad419b0852ef6805b4631259796de660cf94a98
Zotigh <lzotigh1@cnm.edu>
2023-05-03 21:46:45 -0600

    Removing comments

--- Final_API_DEMO/Program.cs
+++ Final_API_DEMO/Program.cs
@@ -44,55 +44,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//Creates HTTP GET End Point
-/*app.MapGet("/helloworld", () => "HelloWorld");*/
-
-//Creates HTTP GET End Point with multiple facets of information.
-/*app.MapGet("/helloworld", () =>
-{
-    //returns Hello World
-    return "Hello World";
-    //INFO you can add calculations and multiple fields here as well.
-});*/
-
-
-//Creates HTTP GET End Point with a bad request
-/*app.MapGet("/helloworld", () =>
-{
-    //Returns a bad request   
-    return Results.BadRequest("Exception");
-});*/
-
-
-/*This is used to pass something to the paramater in a GET request
-* such as a int or a string parameter to pass data.
-* This will allow the server to add an ID to this data object.
-* This only accepts the type you put for example if you use a string
-* Swagger will throw an error (for this example).
-*/
-/*app.MapGet("/helloworld{id}", (int id) =>
-{
-    return Results.Ok("Id!!" + id);
-});*/
-
-//Same as above but this one explicitly says that the type needs to be an int.
-//This is the better way since the other throws a 400 instead of a 404 like it should when using a string.
-/*app.MapGet("/helloworld{id:int}", (int id) =>
-{
-    return Results.Ok("Id!!" + id);
-});*/
-
-//Keeps this request in the same line by telling the server it is
-////You can also do the same thing here with the above get request"Ok".
-//app.MapPost("/helloworld2", () => Results.Ok("Hello World 2 example"));
-
-//Creates HTTP POST End Point
-//app.MapPost("/helloworld2", () => "Hello World 2");
-
-//All of the basic methods such as MAP, DELETE etc.. can be envoked the same way.
-
-//Returns the list of coupons from the data folder ideal again we want to be using a database.
-//Added the WithName function as well
 app.MapGet("/api/coupon", (ILogger<Program> _logger) =>
 {
     APIResponse response = new();
