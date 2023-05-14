511017b0f52ec3efc8af1a1955934273a66d4fce
Zotigh <lzotigh1@cnm.edu>
2023-05-13 23:49:49 -0600

    Header.

--- Final_API_DEMO/MappingConfig.cs
+++ Final_API_DEMO/MappingConfig.cs
@@ -1,4 +1,10 @@
-﻿using AutoMapper;
+﻿/******************************************************
+* Programmer: Lance Zotigh (lzotigh1@cnm.edu/lzotigh1@gmail.com)
+* Program: API Demo
+* Purpose: Demo that shows how to create a Minimal API
+********************************************************/
+
+using AutoMapper;
 using DemoAPI.Models;
 using DemoAPI.Models.DTO;
 
