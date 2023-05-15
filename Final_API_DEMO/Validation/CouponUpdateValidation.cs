ba76365f1a05558b21150d7710eb3d096c13d156
Zotigh <lzotigh1@cnm.edu>
2023-05-14 22:43:47 -0600

    Doc clean-up.

--- Final_API_DEMO/Validation/CouponUpdateValidation.cs
+++ Final_API_DEMO/Validation/CouponUpdateValidation.cs
@@ -14,10 +14,7 @@ namespace DemoAPI.Validation
     {
         public CouponUpdateValidation()
         {
-            //Defines what the rules are for.
-
-            //Defines that the Id cannot be zero and must be greater than 0.
-            RuleFor(model => model.Id).NotEmpty().GreaterThan(0);
+             RuleFor(model => model.Id).NotEmpty().GreaterThan(0);
 
             //Defines rule will not be empty.
             RuleFor(model => model.Name).NotEmpty();
