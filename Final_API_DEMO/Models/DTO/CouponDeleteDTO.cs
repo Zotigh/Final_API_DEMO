8c8580088687697cae7a7452b326b34b471b64ac
Zotigh <lzotigh1@cnm.edu>
2023-05-17 23:28:46 -0600

    Doc clean up.

--- Final_API_DEMO/Models/DTO/CouponDeleteDTO.cs
+++ Final_API_DEMO/Models/DTO/CouponDeleteDTO.cs
@@ -11,6 +11,7 @@ namespace DemoAPI.Models.DTO
         public int Id { get; set; }
 
         public string? Name { get; set; }
+
         public int Percent { get; set; }
         public bool IsActive { get; set;}
     }
