7251e708672fa107585d98b9925ff1df68ead4b8
Zotigh <lzotigh1@cnm.edu>
2023-05-20 01:55:36 -0600

    Code Dlean up

--- Final_API_DEMO/Models/APIResponse.cs
+++ Final_API_DEMO/Models/APIResponse.cs
@@ -14,7 +14,6 @@ namespace DemoAPI.Models
             ErrorMessages = new List<string>();
         }
         public bool IsSuccess { get; set; }
-
         public Object Result { get; set; }
         public HttpStatusCode StatusCode { get; set; }
         public List<string> ErrorMessages { get; set; }
